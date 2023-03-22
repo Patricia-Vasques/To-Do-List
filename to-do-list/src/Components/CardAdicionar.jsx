9ac702df9b99b229987ee8043da2e01428b8ca3c
Patrícia Vasques <patriciahhhh@gmail.com>
2023-03-21 21:21:03 -0300

    criando componentes

---
+++ to-do-list/src/Components/CardAdicionar.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function CardAdicionar{(adicionarTarefa, tarefaFinalizada)} {
+    const [texto, setTexto] = useState
+}
+
+
+return(
+    <div>
+        <input type="text" placeholder="Adicionar nova tarefa" value={texto} onChange={(e) => setTexto(e)} />
+    </div>
+)
+
