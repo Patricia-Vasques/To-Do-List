1a82da47b81287ee1cab772d65f385d07b96ffa9
Patrícia Vasques <patriciahhhh@gmail.com>
2023-03-21 21:28:30 -0300

    criando componentes

--- to-do-list/src/Components/CardAdicionar.jsx
+++ to-do-list/src/Components/CardAdicionar.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 
-function CardAdicionar{(adicionarTarefa, tarefaFinalizada)} {
-    const [texto, setTexto] = useState
+function CardAdicionar ({adicionarTarefa, filtrarTarefasFinalizadas, tarefasFinalizadas}) {
+    const [texto, setTexto] = useState('')
+
+    
+    return(
+    <div>
+        <input type="text" placeholder="Adicionar nova tarefa" value={texto} onChange={(e) => setTexto(e.target.value)} />
+        <button onClick={handleAdicionar}> Adicionar</button>
+        <input type="checkbox" checked={tarefasFinalizadas} onChange={e =>filtrarTarefasFinalizadas(e.target.checked)} />
+        <label>filtrar tarefas finalizadas</label>
+    </div>
+    )
 }
 
 
-return(
-    <div>
-        <input type="text" placeholder="Adicionar nova tarefa" value={texto} onChange={(e) => setTexto(e)} />
-    </div>
-)
+export default CardAdicionar;
 
