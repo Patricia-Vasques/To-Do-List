5681a1e529b52f8f8257937c56924e15f57e6f3e
Patrícia Vasques <patriciahhhh@gmail.com>
2023-03-31 16:41:45 -0300

    Criando uma useState

--- to-do-list/src/App.jsx
+++ to-do-list/src/App.jsx
@@ -1,8 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import CardAdicionar from './Components/CardAdicionar'
 
+function App() {
+    //estado base
+    const [listaDeTarefas, setListaDeTarefas]= useState([
+        {id: 1, texto: "Tarefa 1", finalizado:false}
+    ])
+
+ // função para adionar tarefa
+
+function adicionarTarefa(texto){
+    const novaTarefa = {
+        id: setListaDeTarefas.length +1, 
+        texto: tarefa,
+        finalizado: false
+    }
+
+    setListaDeTarefas([...listaDeTarefas, novaTarefa])
+}
+
+    return(
+        <div className = "App">
+            <CardAdicionar adicionarTarefa={adicionarTarefa}/>
+        </div>
+    )
+}
 
 
 export default App
--- to-do-list/src/Components/CardAdicionar.jsx
+++ to-do-list/src/Components/CardAdicionar.jsx
@@ -1,19 +1,35 @@
 import { useState } from 'react'
 
-function CardAdicionar ({adicionarTarefa, filtrarTarefasFinalizadas, tarefasFinalizadas}) {
-    const [texto, setTexto] = useState('')
+function CardAdicionar ({adicionarTarefa}) {
+    //Criando um useState
+    const [tarefa, setTarefa] = useState('')
 
+    //Criando uma função para adicionar uma tarefa
+
+    function handleAdicionar  (event)  {
+
+        event.preventDefault()
+
+        //Verifica se o input de texto não esta vazio
+        if (tarefa!=""){
+
+            //Cria o texto da tarefa e adiciona na lista
+            adicionarTarefa(tarefa)
+            //limpar o input do texto
+            setTarefa("")
+        }
+    }
     
     return(
     <div>
-        <input type="text" placeholder="Adicionar nova tarefa" value={texto} onChange={(e) => setTexto(e.target.value)} />
-        <button onClick={handleAdicionar}> Adicionar</button>
-        <input type="checkbox" checked={tarefasFinalizadas} onChange={e =>filtrarTarefasFinalizadas(e.target.checked)} />
-        <label>filtrar tarefas finalizadas</label>
+        <input type="text" 
+        placeholder="Digite uma tarefa"  
+        value={tarefa} 
+        onChange={(e) => setTarefa(e.target.value)}/>
+
+        <button onClick={handleAdicionar}> Adicionar </button>
     </div>
-    )
+    );
 }
 
-
 export default CardAdicionar;
-
