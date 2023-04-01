a5eb776b212097765dcb0612bdf51420d9548a90
Patrícia Vasques <patriciahhhh@gmail.com>
2023-03-31 22:03:54 -0300

    exercicio 6 ok

--- src/context/ToDoProvider.jsx
+++ src/context/ToDoProvider.jsx
@@ -2,7 +2,43 @@ import { createContext } from "react";
 
 export const todoContext = createContext();
 
-export const ToDoProvider = ({ children }) => {
+export const ToDoProvider = ( props ) => {
+    const [toDos, setToDos] = useState([
+        { id: 1, text: "Estudar React", done: false },
+        { id: 2, text: "Praticar com um projeto", done: true },
+        { id: 3, text: "Divulgar meu resultado no LinkedIn", done: false },
+      ]);
+    
+      const addToDo = (text) => {
+        const newToDo = {
+          id: Date.now(),
+          text,
+          done: false,
+        };
+        setToDos([...toDos, newToDo]);
+      };
+    
+      const markTodo = (id) => {
+        const newToDos = toDos.map((todo) => {
+          if (todo.id === id) {
+            return { ...todo, done: !todo.done };
+          }
+          return todo;
+        });
+        setToDos(newToDos);
+      };
+    
+      const completedToDos = toDos.filter((todo) => todo.done);
+      const pendingToDos = toDos.filter((todo) => !todo.done);
+
+      const contextValues = {
+        toDos,
+        addToDo,
+        markTodo,
+        completedToDos,
+        pendingToDos,
+      };
+      
     return(
         <todoContext.Provider value = {undefined} > {props.children} </todoContext.Provider>
     )
